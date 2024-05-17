3be0932250ea8650b88157c22d29ca33b783fe1d
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-05-17 12:35:22 +0100

    solving challenge 3

--- src/3.ts
+++ src/3.ts
@@ -28,6 +28,50 @@
 ]
 **/
 
-export function findByType(pokedex, type) {
-	// ???
+type Pokemon = {
+	id: number,
+	name: string,
+	type: Array<string>,
+	height: string,
+	weight: string,
+	weaknesses: Array<string>
+
 }
+
+export function findByType(pokedex: Array<Pokemon>, type: string) {
+	let pokemon = [];
+	for (let p of pokedex) {
+		if (p.type.includes(type)) pokemon.push(p);
+	}
+	return pokemon;
+}
+
+let examplePokedex = [
+	{
+		"id": 3,
+		"name": "Venusaur",
+		"type": ["Grass", "Poison"],
+		"height": "2.01 m",
+		"weight": "100.0 kg",
+		"weaknesses": ["Fire", "Ice", "Flying", "Psychic"]
+	},
+	{
+		"id": 6,
+		"name": "Charizard",
+		"type": ["Fire", "Flying"],
+		"height": "1.70 m",
+		"weight": "90.5 kg",
+		"weaknesses": ["Water", "Electric", "Rock"]
+	},
+	{
+		"id": 9,
+		"name": "Blastoise",
+		"type": ["Water"],
+		"height": "1.60 m",
+		"weight": "85.5 kg",
+		"weaknesses": ["Electric", "Grass"]
+	}
+]
+
+console.log(findByType(examplePokedex, "Water"));
+console.log(findByType(examplePokedex, "Grass"));
